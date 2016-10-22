67c545f2d6a33abc0f48c1b9f5563b7f1237717d
Dorian Jaminais <dorian@jaminais.fr>
2016-10-21 19:04:19 -0700

    Improve metadata

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'users'
 depends          'openssh'
+source_url       'https://github.com/nanassito/chef-survivor'
+issues_url       'https://github.com/nanassito/chef-survivor/issues'
